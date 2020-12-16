c2bdc4c36d570f845f52818c690ccc1a5126a693
r10t-b0y <75988829+r10t-b0y@users.noreply.github.com>
2020-12-16 08:42:15 +0700

    Update belajar.py

--- belajar.py
+++ belajar.py
@@ -3,10 +3,10 @@ print "Selamat Datang Di Grup Saya"
 print "Member Baru Wajib Intro"
 
 # Input
-nama = raw_input ("Masukkan Nama Samaran Anda := ")
-umur = input ("Masukkan Usia Anda := ")
-asal = raw_input ("Masukkan Alamat Anda := ")
-contact = input ("Masukkan Nomer Anda {contoh 6285xxxxxxxxx}:= ")
+nama = input ("Masukkan Nama Samaran Anda := ")
+umur = int(input("Masukkan Usia Anda := "))
+asal = input ("Masukkan Alamat Anda := ")
+contact = int(input("Masukkan Nomer Anda {contoh 6285xxxxxxxxx}:= "))
 
 # Output
 print "Nama = ",nama,",","Usia = ",umur
@@ -14,10 +14,11 @@ print "Nama = ",nama,",","Usia = ",umur
 # Jika tidak cukup umur
 if(umur < 18):
     print "Maaf Usia Anda Masih Belum Mencukupi Syarat Untuk Masuk Grup Saya"
-    quit()
+    sys.exit()
 
 # Jika cukup umur
 if(umur >= 18):
     print "Selamat Anda Telah Mencukupi Syarat Untuk Masuk Grup Saya"
     print "Semoga Tidak Betah :V"
     print "Link Grup = https://chat.wahatsapp.com/AJU875H2J"
+    
